0e0fc02863c13e1eac145f50d26ad1520f38b149
dave <daveje007@googlemail.com>
2020-10-27 22:09:09 +0000

    azure commit

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
-variable "myvar" {
-  type = string
-  default = "hello world"
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
 }
